92ff5b80ff0422bca2046172111755760f84f99c
John O'Grady <johndj.og@gmail.com>
2017-11-20 19:01:58 +0000

    Added in random spawn logic as well as rotation checking

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+	string[] names = {"Prefabs/Tetromino_T", "Prefabs/Tetromino_I", "Prefabs/Tetromino_J", "Prefabs/Tetromino_bs", "Prefabs/Tetromino_s", "Prefabs/Tetromino_Sq"};
     public static int gridHeight= 22;
     public static int gridWidth = 10;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		generateNext();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 
-    public bool CheckIsInsideGrid(Vector3 position)
+	public void generateNext()
+	{
+		GameObject instance = (GameObject)Instantiate(Resources.Load(names[Random.Range(0, 5)], typeof(GameObject)), new Vector3(5.0f, 20.0f, -3.0f), Quaternion.identity);
+	}
+
+    public bool CheckIsInsideGrid(Vector2 position)
     {
         return ((int)position.x >= 0 && (int)position.x < gridWidth && (int)position.y >= 0);
     }
 
-    public Vector2 Round(Vector3 pos)
+    public Vector2 Round(Vector2 pos)
     {
         return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
     }
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -7,11 +7,15 @@ public class Tetromino : MonoBehaviour {
     float fall = 0;
     public float fallSpeed = 1;
 
+	public bool enableRotation = true;
+	public bool disableRotation = false;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	    
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
         CheckInput();
@@ -19,34 +23,73 @@ public class Tetromino : MonoBehaviour {
 
     void CheckInput()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(1,0,0);
-            if(!CheckIsValidPosition())
-            {
-                transform.position += new Vector3(-1, 0, 0);
-            }
-        }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-            if (!CheckIsValidPosition())
-            {
-                transform.position += new Vector3(1, 0, 0);
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.Rotate(0,0,90);
-        }
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			transform.position += new Vector3 (1, 0, 0);
+			if (!CheckIsValidPosition ()) {
+				transform.position += new Vector3 (-1, 0, 0);
+			}
+		} 
+
+		else if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			transform.position += new Vector3 (-1, 0, 0);
+			if (!CheckIsValidPosition ()) {
+				transform.position += new Vector3 (1, 0, 0);
+			}
+		} 
+
+		else if (Input.GetKeyDown (KeyCode.UpArrow)) 
+		{
+			if (enableRotation) 
+			{
+				if (disableRotation) 
+				{
+					if (transform.rotation.eulerAngles.z >= 90) 
+					{
+						transform.Rotate (0, 0, -90);
+					} 
+					else 
+					{
+						transform.Rotate (0, 0, 90);
+					}
+				} 
+				else 
+				{
+					transform.Rotate (0, 0, 90);
+				}
+
+				if (!CheckIsValidPosition ()) 
+				{
+					if (disableRotation) 
+					{
+						if (transform.rotation.eulerAngles.z >= 90)
+						{
+							transform.Rotate (0, 0, -90);
+						} 
+						else 
+						{
+							transform.Rotate (0, 0, 90);
+						}
+					} 
+					else 
+					{
+						transform.Rotate (0, 0, -90);
+					}
+				} 
+			}
+		}
+
         else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
         {
             transform.position += new Vector3(0, -1, 0);
             fall = Time.time;
-            if (!CheckIsValidPosition())
-            {
-                transform.position += new Vector3(0, 1, 0);
-            }
+
+			if (!CheckIsValidPosition())
+			{
+				transform.position += new Vector3(0, 1, 0);
+			}
         }
     }
 
