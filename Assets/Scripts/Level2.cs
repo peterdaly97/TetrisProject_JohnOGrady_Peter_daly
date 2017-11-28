824202bc965c503be61604f602ea3c79ee8a9f56
Peter <c00205483@itcarlow.ie>
2017-11-28 13:58:35 +0000

    Added a second level

---
+++ Assets/Scripts/Level2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level2 : MonoBehaviour {
+
+	public void LoadGame()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+}
